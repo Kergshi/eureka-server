ead19065d4a750b06b7b4419298bed9c8da2452f
Fedor Shtanko <atraxlxxxl@gmail.com>
2024-05-19 21:43:09 +0300

    new2

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,16 +19,10 @@ extra["springCloudVersion"] = "2023.0.1"
 
 dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 dependencyManagement {
     imports {
         mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
     }
-}
-
-tasks.withType<Test> {
-    useJUnitPlatform()
 }
